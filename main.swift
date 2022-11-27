9fe3c67d8458e5a1d44755e2b853351e1486bddc
Brayden Edington <brayden.edington@student.allenisd.org>
2022-11-27 11:33:56 -0600

    Initial commit

---
+++ main.swift
@@ -0,0 +1,21 @@
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+func factorial(num:Int) -> Int {
+    print("Calculating factorial(\(num))...")
+    if num == 1 {
+        print("Terminal case, returning 1.")
+        return 1
+    }else{
+        let result =  num * factorial(num: num - 1)
+        print("Non-terminal case, returning \(result).")
+        return result
+    }
+}
+
+let result = factorial(num:n)
+print("The final result is: \(result).")
