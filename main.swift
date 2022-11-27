2609d8926a826801f17e8f09c15d33d27f9168cc
Brayden Edington <brayden.edington@student.allenisd.org>
2022-11-27 12:07:35 -0600

    Removed logging and global variables

--- main.swift
+++ main.swift
@@ -1,21 +1,13 @@
-// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-let n = 7
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
-
-
 func factorial(num:Int) -> Int {
-    print("Calculating factorial(\(num))...")
+    //If num = 1, this is the terminal case and just return 1
     if num == 1 {
-        print("Terminal case, returning 1.")
         return 1
+    //Else, non-terminal case multiply num and factorial(num - 1)    
     }else{
         let result =  num * factorial(num: num - 1)
-        print("Non-terminal case, returning \(result).")
         return result
     }
 }
 
-let result = factorial(num:n)
-print("The final result is: \(result).")
+print(factorial(num:9))
+
